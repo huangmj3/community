0ba8e02e684c331182b36b56fb741003973c9279
huangmj3 <18292493938@163.com>
2019-07-15 01:18:40 +0800

    add index and bootstrap

--- src/main/java/com/huangmj/community/controller/IndexController.java
+++ src/main/java/com/huangmj/community/controller/IndexController.java
@@ -1,4 +1,13 @@
 package com.huangmj.community.controller;
 
-public class HelloController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    //一个反斜杠代表根目录
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
