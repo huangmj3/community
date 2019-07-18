6f4e8c148531ea7dd2efe8ac6ab44abed0bdefcc
huangmj3 <18292493938@163.com>
2019-07-18 19:42:28 +0800

    publish

---
+++ src/main/resources/db/migration/V3__Create_question_table.sql
@@ -0,0 +1,16 @@
+create table questions
+(
+	id int auto_increment,
+	title varchar(50) null,
+	description text null,
+	gmt_create bigint null,
+	gmt_modified bigint null,
+	creator int null,
+	comment_count int default 0 null,
+	view_count int default 0 null,
+	like_count int default 0 null,
+	tag varchar(256) null,
+	constraint questions_pk
+		primary key (id)
+);
+
